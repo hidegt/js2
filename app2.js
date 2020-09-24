04a26b58651b9d4fadc42183056867c992f1371e
Hideyo Gotoda <hidehide@HideyonoMacBook-Pro.local>
2020-09-24 17:13:56 +0900

    [finish]Java Script2

---
+++ app2.js
@@ -0,0 +1,57 @@
+// var hello = "Hello, World!"
+// alert(hello);
+// var int1 = 1;
+// alert(int1);
+
+// 計算
+// alert(4 + 3);
+// alert(4 - 3);
+// alert(4 * 3);
+// alert(4 / 2);
+// alert(4 % 3);
+
+// 文字列の結合
+// alert("Hello" + "World");
+
+// var str1 = "Hi,";
+// var str2 = "world!!!!";
+// alert(str1 + str2);
+
+// 条件分岐
+// var orange = 100;
+// var apple = 120;
+
+// if(orange < apple){
+//   alert("みかんの値段がりんごより安い");
+// }else if(orange == apple){
+//   alert("みかんとりんごが同じ値段");
+// }else{
+//   alert("みかんの値段がりんごより高い");
+// }
+
+// while
+// var max = 100;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+//   num = num *2;
+//   count = count +1;
+// }
+// alert("2を掛け続けて" + max + "を超えるのに必要だった回数は" + count + "回です");
+
+// for
+// var i;
+// var num = 0;
+// for(i = 1; i<11; i++){
+//   num = num + i;
+// }
+// alert("1から10まで足し算した結果は" + num + "です");
+
+// practice
+var num1 = 8;
+var num2 = 4;
+var num3 = 5;
+var num4 = num1 + num2
+var num5 = num1 - num2
+alert("8+4=" + num4 + "\n8-4=" + num5 + "\n8x4=" + num1 * num2 + "\n8/4=" + num1/num2 + "\n8%5=" + num1 % num3);
